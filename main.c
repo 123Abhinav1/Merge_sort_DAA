414c706baf5012507920dd11855bb4f55257bcb7
ABHINAV KUMAR <115417397+123Abhinav1@users.noreply.github.com>
2023-04-21 11:31:19 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,3 @@
-
-  
-  /*
-  
-  #pseudocode
-  MergeSort(arr, left, right):
-    if left > right 
-        return
-    mid = (left+right)/2
-    mergeSort(arr, left, mid)
-    mergeSort(arr, mid+1, right)
-    merge(arr, left, mid, right)
-end */
-
-
-
-
-
-
 #include <iostream>
 using namespace std;
 
